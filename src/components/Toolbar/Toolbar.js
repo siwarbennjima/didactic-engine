1bb63fdc06ec76f4d1d1653b44d389a02c1e4f65
Wael <waelben7@gmail>
2020-02-25 18:45:52 +0100

    css and img

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import NavigationItems from './NavigationItems/NavigationItems'
+import classes from './Toolbar.module.css'
+import burgerLogo from '../../assets/images/logo.png'
+
 const toolbar = () => {
   return (
-    <div>
-      menu, logo
+    <div className={classes.Toolbar}>
+      <div>MENU</div>
+      <div className={classes.Logo}>
+        <img src={burgerLogo} alt='Magic Burger' />
+      </div>
       <NavigationItems />
     </div>
   )
